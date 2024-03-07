a6ef0cff34384397a6aa0ca4323c6bdfe3c625b3
LouisCR04 <louiskariuki04@gmail.com>
2024-03-07 22:18:27 +0300

    Added commas separating dates and time

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,14 +9,14 @@ from fabric.api import local
 def do_pack():
     """Generates a tgz archive from contents of web_static directory"""
 
-    d_time = datetime.utcnow()
+    dt = datetime.utcnow()
 
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(d_time.year,
-                                                         d_time.month
-                                                         d_time.day
-                                                         d_time.hour
-                                                         d_time.minute
-                                                         d_time.second)
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
 
     if os.path.isdir("versions") is False:
         if local("mkdir -p versions").failed is True:
