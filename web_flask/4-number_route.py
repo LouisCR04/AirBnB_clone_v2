97266c9aeb7488560b6450966c2e9cc2f13279d2
LouisCR04 <louiskariuki04@gmail.com>
2024-03-22 14:28:37 +0300

    /number/<int:n> added

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-"""
-Displays “Python ”, followed by
-the value of the text variable
-(replace underscore _ symbols with a space )
-"""
+"""Display “n is a number” only if n is an integer"""
 
 from flask import Flask
 
@@ -30,6 +26,11 @@ def c_text(text):
 @app.route("/python/<text>", strict_slashes=False)
 def python(text="is cool"):
     return f"Python {text.replace('_', ' ')}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    return f"{n} is a number"
 
 
 if __name__ == "__main__":
